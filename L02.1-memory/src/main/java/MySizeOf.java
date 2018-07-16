76e3f751cbb18f50313b9b8d4d19dcf18efd3f9b
DenisAbl <ablamskiy.d@gmail.com>
2018-07-16 21:19:54 +0300

    ready for check

--- L02.1-memory/src/main/java/MySizeOf.java
+++ L02.1-memory/src/main/java/MySizeOf.java
@@ -2,12 +2,13 @@ import java.util.function.Supplier;
 
 public class MySizeOf {
 
-
-    final static int NUMBER_OF_OBJECTS = 20_000_000;
+    private final static int NUMBER_OF_OBJECTS = 20_000_000;
     private static long startMemory = 0;
     private static long finishMemory = 0;
 
-    public static void sizeOf(Supplier<?> supplier){
+ //Метод на вход принимает в качестве фабрики реализацию функционального интрефейса Supplier
+
+    public static void sizeOf(Supplier<?> supplier) throws InterruptedException {
 
         startMemory = getUsedMemory();
         Object[] arrayOfObjects = new Object[NUMBER_OF_OBJECTS];
@@ -15,19 +16,18 @@ public class MySizeOf {
         System.out.println("Reference size: " + (finishMemory - startMemory)/arrayOfObjects.length + " bytes");
 
         startMemory = getUsedMemory();
+        for (int i = 0; i < arrayOfObjects.length; i++){
+             arrayOfObjects[i] = supplier.get(); }
+        finishMemory = getUsedMemory();
+        System.out.println(supplier.get().getClass().getTypeName() + " size: " + (finishMemory - startMemory)/arrayOfObjects.length + " bytes" +'\n');
 
-         for (int i = 0; i < arrayOfObjects.length; i++){
-             arrayOfObjects[i] = supplier.get();
-         }
-         finishMemory = getUsedMemory();
-
-        System.out.println("Object size: " + (finishMemory - startMemory)/arrayOfObjects.length + " bytes");
+        arrayOfObjects = null;
     }
 
-    private static long getUsedMemory() {
+    private static long getUsedMemory() throws InterruptedException {
         System.gc();
         Runtime runtime = Runtime.getRuntime();
-        return (runtime.totalMemory() - runtime.getRuntime().freeMemory());
+        return (runtime.totalMemory() - runtime.freeMemory());
     }
 
 }
--- L02.1-memory/src/main/java/TestMemory.java
+++ L02.1-memory/src/main/java/TestMemory.java
@@ -1,12 +1,27 @@
-import java.util.function.Supplier;
+import java.util.*;
 
 public class TestMemory {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        //Строка не из String Pool; Integer; Object; массив int
 
         MySizeOf.sizeOf(() -> new String(new byte[0]));
         MySizeOf.sizeOf(() -> new Integer(123));
+        MySizeOf.sizeOf(() -> new Object());
+        MySizeOf.sizeOf(() -> new int[0]);
+        MySizeOf.sizeOf(() -> new int[10]);
+
+        //Пустые контейнеры
+        MySizeOf.sizeOf(ArrayList::new);
+        MySizeOf.sizeOf(LinkedList::new);
+        MySizeOf.sizeOf(HashMap::new);
+        MySizeOf.sizeOf(Stack::new);
+
+        //Коллекция из N элементов
+        MySizeOf.sizeOf(()-> new  ArrayList<Integer>(10));
+
 
     }
 }
